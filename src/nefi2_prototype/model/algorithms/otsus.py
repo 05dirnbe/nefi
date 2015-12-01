02426f731be64940d9ad6716c6dce3acae776ca6
andreasfirczynski <andreasfir91@googlemail.com>
2015-12-01 10:30:36 +0100

    added otsus threshold

--- src/nefi2_prototype/model/algorithms/otsus.py
+++ src/nefi2_prototype/model/algorithms/otsus.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import cv2 as cv
 
 __algorithm__ = 'Otsus Threshold'
 __belongs2__ = 'Segmentation'
 
+#Segmentation routines
+THRESHOLD_FG_COLOR = 255
+THRESHOLD_BG_COLOR = 0
 
 def process(image, settings):
     """Replace this stub with actual implementation."""
@@ -39,6 +43,20 @@ def sign(image, settings):
     """
     image.sign(__algorithm__, settings)
 
+def otsus_threshold(image, threshold_value=0, threshold_type=cv.THRESH_OTSU):
+    """
+    Computes a threshold image of the source image using the otsus threshold value.
+
+    Args:
+        Image: An input image which will not be changed
+
+    Returns:
+        A threshold image
+    """
+    grayscale_image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+    threshold_image = cv.threshold(grayscale_image, threshold_value, THRESHOLD_FG_COLOR, threshold_type)[1]
+
+    return threshold_image
 
 if __name__ == '__main__':
     pass
