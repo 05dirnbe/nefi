8765fb89ed2bfa2c26812945b5d3229b22a4f076
PhilippR <s9phreic@stud.uni-saarland.de>
2015-12-17 18:59:41 +0100

    .

--- gui_prototypes/Dennis/GuiTester.py
+++ gui_prototypes/Dennis/GuiTester.py
@@ -1,36 +1,36 @@
-from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
-from PyQt5.QtCore import pyqtSignal, Qt
-from PyQt5.QtWidgets import (QApplication, QBoxLayout, QCheckBox, QComboBox,
-                             QDial, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QScrollBar,
-                             QSlider, QSpinBox, QStackedWidget, QWidget)
-import algorithm_test
-
 import sys
+
+from PyQt5.QtCore import pyqtSignal, Qt
+from PyQt5.QtWidgets import (QApplication, QBoxLayout, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QSlider, QSpinBox, QStackedWidget, QWidget)
+
+from gui_prototypes.Philipp import algorithm_test
 
 
 class GuiController:
     def __init__(self, algorithm):
         self.app = QApplication(sys.argv)
-        self.window = Window()
-        self.signals = None
-        self.horizontal_sliders = self.window.horizontalSliders
+
+        self.window = Window(algorithm.get_name() + ":")
+        self.algorithm_sliders = SlidersGroup(algorithm.get_name())
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.controlsGroup)
+        layout.addWidget(self.stackedWidget)
+        self.setLayout(layout)
 
         for item in algorithm.integer_sliders:
-            slider = self.horizontal_sliders.add_slider(item.default, item.lower, item.upper)
-            SlidersGroup("test")
-            slot = item.set_value
-            signal = pyqtSignal()
-            # slider.connect(signal)
+            slider = self.algorithm_sliders.add_slider_box(item.name, item.default, item.lower, item.upper)
+            # textfield = self.horizontal_sliders.add_textfield(item.default, item.lower, item.upper)
 
         self.window.show()
         sys.exit(self.app.exec_())
 
 
 class Window(QWidget):
-    def __init__(self):
+    def __init__(self, title):
         super(Window, self).__init__()
 
-        self.horizontalSliders = SlidersGroup("test")
+        self.horizontalSliders = SlidersGroup(title)
 
         self.stackedWidget = QStackedWidget()
         self.stackedWidget.addWidget(self.horizontalSliders)
@@ -39,7 +39,7 @@ class Window(QWidget):
         layout.addWidget(self.stackedWidget)
         self.setLayout(layout)
 
-        self.setWindowTitle("Sliders")
+        self.setWindowTitle("Algorithm Settings: ")
 
 
 class SlidersGroup(QGroupBox):
@@ -47,34 +47,57 @@ class SlidersGroup(QGroupBox):
 
     def __init__(self, title, parent=None):
         super(SlidersGroup, self).__init__(title, parent)
-        self.slidersLayout = QBoxLayout(QBoxLayout.LeftToRight)
+        self.slidersLayout = QBoxLayout(QBoxLayout.TopToBottom)
         self.setLayout(self.slidersLayout)
 
-    def add_slider(self, default, lower, upper):
+    # SliderBox describes a group of a slider, a textfield and a title
+    def add_slider_box(self, name, default, lower, upper):
+
+        """
+
+        Args:
+            name:
+            default:
+            lower:
+            upper:
+
+        Returns:
+            object:
+
+        """
+
+        slider_box = QGroupBox(name)
+        slider_box_layout = QGridLayout()
+
+        # Slider itself
         slider = QSlider(Qt.Horizontal)
         slider.setFocusPolicy(Qt.StrongFocus)
         slider.setTickPosition(QSlider.TicksBothSides)
         slider.setRange(lower, upper)
         slider.setValue(default)
-        slider.setTickInterval(10)
-        slider.setSingleStep(1)
-        self.slidersLayout.addWidget(slider)
-        return slider
+        #slider.setTickInterval(10)
+        slider.setSingleStep(upper-lower)
 
+        # Textfield
+        textfield = QSpinBox()
+        textfield.setRange(lower, upper)
+        textfield.setSingleStep(1)
+        textfield.setValue(default)
 
-def createControls(self, title):
-    self.controlsGroup = QGroupBox(title)
+        # Label
+        title = QLabel("" + name + ": ")
 
-    valueLabel = QLabel("Current value:")
+        # Connect Textfield with Slider
+        textfield.valueChanged.connect(slider.setValue)
+        slider.valueChanged.connect(textfield.setValue)
 
-    self.valueSpinBox = QSpinBox()
-    self.valueSpinBox.setRange(-100, 100)
-    self.valueSpinBox.setSingleStep(1)
+        # Arrange Title, Slider and Textfield on the Box
+        slider_box_layout.addWidget(title, 0, 0)
+        slider_box_layout.addWidget(slider, 0, 1)
+        slider_box_layout.addWidget(textfield, 0, 2)
+        slider_box.setLayout(slider_box_layout)
 
-    controlsLayout = QGridLayout()
-    controlsLayout.addWidget(valueLabel, 2, 0)
-    controlsLayout.addWidget(self.valueSpinBox, 2, 1)
-    self.controlsGroup.setLayout(controlsLayout)
+        return slider_box
 
 
 MyAlgorithm = algorithm_test.MyAlgorithm
