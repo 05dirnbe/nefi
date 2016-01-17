b158c0711431b272acfbd6aab7b73b7aec50af53
Martino Bruni <s8mobrun@stud.uni-saarland.de>
2016-01-17 19:02:04 +0100

    Updated unittest for Extension Loader and Pipeline

--- nefi2/unittests/unittest_model/Test_Extension_Loader.py
+++ nefi2/unittests/unittest_model/Test_Extension_Loader.py
@@ -15,29 +15,32 @@ from collections import OrderedDict as od, OrderedDict
 
 from nefi2.model.ext_loader import ExtensionLoader
 import sys
-sys.path.insert(0, os.path.join('..','..', 'model'))
-sys.path.insert(0, os.path.join('..','..', 'model', 'categories'))
-sys.path.insert(0, os.path.join('..','..', 'model', 'algorithms'))
+sys.path.insert(0, os.path.join(os.pardir,os.pardir, 'model'))
+sys.path.insert(0, os.path.join(os.pardir,os.pardir, 'model', 'categories'))
+sys.path.insert(0, os.path.join(os.pardir,os.pardir,'model', 'algorithms'))
+
+
 
 class Test_Extension_Loader(unittest.TestCase):
 
 
     def test_scand_model(self):
         """
-        The List standard will be the result of the method _scan_model(cat_dir) if the cat_dir is './model/categories'
+        The List standard should be the result of the method _scan_model(cat_dir) if the cat_dir is '../../model/categories'
         """
-        print (sys.path)
         ext_Loader = ExtensionLoader()
         standard = ['preprocessing.py', 'segmentation.py', 'gdetection.py', 'gfiltering.py']
-        cat_dir = os.path.join(os.curdir, 'model', 'categories')
-        self.assertEqual(self.ext_Loader._scan_model(cat_dir),standard)
+        cat_dir = os.path.join(os.pardir,os.pardir, 'model', 'categories')
+        self.assertEqual(ext_Loader._scan_model(cat_dir),standard)
 
     def test_read_config(self):
-        print (sys.path)
+        """
+        The List standard should be the result of the method read_config(config_path) if the config_path is the path of the file config.xml
+        """
         ext_Loader = ExtensionLoader()
-        standard = ['Preprocessing', 'Segmentation','Graph Detection', 'Graph Filtering']
-        self.assertEqual(self.ext_Loader._read_configs(),standard)
-        self.ext_Loader._instantiate_cats()
+        standard = ['Preprocessing', 'Segmentation','Graph detection', 'Graph filtering']
+        config_dir = os.path.join(os.pardir,os.pardir,'model','config.xml')
+        self.assertEqual(ext_Loader._read_configs(config_dir),standard)
 
 
 """
--- nefi2/unittests/unittest_model/Test_Pipeline.py
+++ nefi2/unittests/unittest_model/Test_Pipeline.py
@@ -1,77 +1,4 @@
-<<<<<<< HEAD
-__author__ = 'martino'
-import unittest
-import sys
-import os
-from model.pipeline import Pipeline
-from model.ext_loader import ExtensionLoader
-
-sys.path.insert(0, os.path.join(os.curdir,'nefi2'))
-sys.path.insert(0, os.path.join(os.curdir,'nefi2', 'model'))
-sys.path.insert(0, os.path.join(os.curdir,'nefi2', 'model', 'categories'))
-sys.path.insert(0, os.path.join(os.curdir,'nefi2', 'model', 'algorithms'))
-
-class Test_Pipeline(unittest.TestCase):
-
-    def test_new_Category(self):
-        """
-        Method new_Category will return True after the creation of the instance of Category's object at position x
-        """
-        extloader = ExtensionLoader()
-        pipeline = Pipeline(extloader.cats_container)
-        self.assertTrue(Pipeline.new_category(1))
-
-    def test_move_category(self):
-        """
-        Method move_Category will return True after the moving of Category's objects from position x to position y
-        """
-        extloader = ExtensionLoader()
-        pipeline = Pipeline(extloader.cats_container)
-        self.assertTrue(pipeline.move_category(1,1))
-
-    def test_delete_category(self):
-        """
-        Method delete_Category will return True after the deleting of Category's object at position x
-        """
-        extloader = ExtensionLoader()
-        pipeline = Pipeline(extloader.cats_container)
-        self.assertTrue(pipeline.delete_category(0))
-
-    def test_change_algorithm(self):
-        """
-        Method change_algorithm will return True for changing the modified attribute of the algorithm at the Step x
-        """
-        extloader = ExtensionLoader()
-        pipeline = Pipeline(extloader.cats_container)
-        self.assertTrue(pipeline.change_algorithm(1,'Blur'))
-
-    def test_get_executed_cats(self):
-        """
-        Method get_executed_cats will return the list of executed categories and the test will compare it with a set of standard ones
-        """
-        extloader = ExtensionLoader()
-        pipeline = Pipeline(extloader.cats_container)
-        standard = 'Preprocessing' #check which executed cats
-        self.assertEqual(pipeline.get_executed_cats(),standard)
-
-    def test_get_algorithm_list(self):
-        """
-        Method get_algorithm_list will return the list of available algorithms the test will compare it wiht the algorithms available for the Preprocessing categories
-        """
-        extloader = ExtensionLoader()
-        pipeline = Pipeline(extloader.cats_container)
-        standard = ['Blur','Bilateral','Color enhancement','Fast nl Means Denoising','Fast nl Means Denoising Colored','Gaussian Blur','Invert Color','Median Blur']
-        self.assertEqual(pipeline.get_algorithm_list(1),standard)
-
-    def test_read_pipeline_xml(self):
-        ""
-        extloader = ExtensionLoader()
-        pipeline = Pipeline(extloader.cats_container)
-
-if __name__ == '__main':
-=======
 #!/usr/bin/env python3
-
 
 __authors__ = {"Martino Bruni": "bruni.martino92@gmail.com"}
 
@@ -83,10 +10,18 @@ from nefi2.model.ext_loader import ExtensionLoader
 from nefi2.model.categories._category import Category
 
 
-sys.path.insert(0, os.path.join(os.curdir,'nefi2'))
-sys.path.insert(0, os.path.join(os.curdir,'nefi2', 'model'))
-sys.path.insert(0, os.path.join(os.curdir,'nefi2', 'model', 'categories'))
-sys.path.insert(0, os.path.join(os.curdir,'nefi2', 'model', 'algorithms'))
+sys.path.insert(0, os.path.join(os.pardir,os.pardir,'nefi2'))
+sys.path.insert(0, os.path.join(os.pardir,os.pardir, 'model'))
+sys.path.insert(0, os.path.join(os.pardir,os.pardir, 'model', 'categories'))
+sys.path.insert(0, os.path.join(os.pardir,os.pardir, 'model', 'algorithms'))
+
+for path in sys.path:
+    if path == (os.path.join(os.curdir,'model')):
+        sys.path.remove(path)
+    elif path == (os.path.join(os.curdir,'model','categories')):
+        sys.path.remove(path)
+    elif path == (os.path.join(os.curdir,'model','algorithms')):
+        sys.path.remove(path)
 
 class Test_Pipeline(unittest.TestCase):
 
@@ -96,8 +31,8 @@ class Test_Pipeline(unittest.TestCase):
         """
         extloader = ExtensionLoader()
         pipeline = Pipeline(extloader.cats_container)
-        pipeline.new_category(1)
-        standard = Category()
+        pipeline.new_category('Preprocessing' ,1)
+        standard = Category('Preprocessing')
         self.assertEqual(pipeline.executed_cats[1],standard)
 
     def test_move_category(self):
@@ -106,8 +41,8 @@ class Test_Pipeline(unittest.TestCase):
         """
         extloader = ExtensionLoader()
         pipeline = Pipeline(extloader.cats_container)
-        pipeline.new_category(1)
-        pipeline.new_category(2)
+        pipeline.new_category('Preprocessing', 1)
+        pipeline.new_category('Segmentation', 2)
         cat1 = pipeline.executed_cats[1]
         cat2 = pipeline.executed_cats[2]
         pipeline.move_category(1,2)
@@ -154,5 +89,4 @@ class Test_Pipeline(unittest.TestCase):
 
 
 if __name__ == '__main':
->>>>>>> aa8258dcb37ef294604cb514e42bc747555a26f0
     unittest.main()
