7c71a0b8f43c8929c13bbee01271e41402dfe821
Martino Bruni <s8mobrun@stud.uni-saarland.de>
2016-01-25 21:40:44 +0100

    minor changes at Edge Attribute filter

--- Martino_Graph_filtering_Algorithm/Edge_Attribute_filter.py
+++ Martino_Graph_filtering_Algorithm/Edge_Attribute_filter.py
@@ -58,7 +58,7 @@ class AlgBody(Algorithm):
 
 
 
-    def process(self, input):
+    def process(self, input_data):
 
         """
         Implements a filter which filters a graph for a certain edge attribute
@@ -71,7 +71,7 @@ class AlgBody(Algorithm):
         Example: Remove all edges with length exaclty 7
 
         Args:
-            | *input* : a list that contain the image and the graph
+            | *input* : a list which contains the image and the graph
         Raises:
             | *KeyError* : Filtering failed because attribute is not present
              in the graph as an edge attribute
@@ -81,10 +81,10 @@ class AlgBody(Algorithm):
         try:
             self.operator.value = self.checkOperator()
             to_be_removed = [(u, v) for u, v, data in
-                             input[1].edges_iter(data=True)
+                             input_data[1].edges_iter(data=True)
                 if self.operator.value(data[self.attribute.value],
                                  self.attribute_threshold_value.value)]
-            input[1].remove_edges_from(to_be_removed)
+            input_data[1].remove_edges_from(to_be_removed)
 
             print ('discarding a total of', len(to_be_removed), 'edges ...')
 
@@ -94,7 +94,7 @@ class AlgBody(Algorithm):
                   ' Filtering failed because', e)
             print ('is not present in the graph as an edge attribute.')
 
-        self.result['graph'] = input[1]
+        self.result['graph'] = input_data[1]
 
 if __name__ == '__main__':
     pass
