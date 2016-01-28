89250ddb6250249b90ba98c1c0df951389362a85
Philipp Reichert <prei@me.com>
2016-01-28 02:44:50 +0100

    Added Pipeline_Order Widget

---
+++ nefi2/view/pipeline_order.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, \
+    QVBoxLayout, QListWidget, QAbstractItemView
+
+
+class PipelinOrder(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.widget_layout = QVBoxLayout()
+
+        # Create ListWidget and add 10 items to move around.
+        self.list_widget = QListWidget()
+        for x in range(1, 11):
+            self.list_widget.addItem('Item {:02d}'.format(x))
+
+        # Enable drag & drop ordering of items.
+        self.list_widget.setDragDropMode(QAbstractItemView.InternalMove)
+
+        self.widget_layout.addWidget(self.list_widget)
+        self.setLayout(self.widget_layout)
