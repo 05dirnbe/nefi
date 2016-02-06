8557be01da1e1548bea7f3416334b7278fa339f2
SebastianS <s9sesct@stu.uni-saarland.de>
2016-02-06 18:02:17 +0100

    removed unnecessary operations

--- nefi2/model/algorithms/otsus.py
+++ nefi2/model/algorithms/otsus.py
@@ -42,12 +42,9 @@ class AlgBody(Algorithm):
 
         """
         gray_img = cv2.cvtColor(args[0], cv2.COLOR_RGB2GRAY)
-        processed_img = cv2.threshold(gray_img, 0, 255,
+        self.result['img'] = cv2.threshold(gray_img, 0, 255,
                                       cv2.THRESH_BINARY_INV +
                                       cv2.THRESH_OTSU)[1]
-        zero_img_arr = np.zeros_like(args[0])
-        zero_img_arr[processed_img == 255] = [255] * 3
-        self.result['img'] = zero_img_arr
 
 if __name__ == '__main__':
     pass
