311433ed6b37e84da69b97166314a78f300796f5
Dennis Groß <gdennis91@googlemail.com>
2016-03-04 12:34:03 +0100

    minor changes

--- nefi2/install_scripts/Win32Py34Dependencies/windows_install.bat
+++ nefi2/install_scripts/Win32Py34Dependencies/windows_install.bat
@@ -1,37 +1,37 @@
 :::::::::::::::::::::::::::::::
 :: Installing NEFI dependencies
 :::::::::::::::::::::::::::::::
-NET SEND %ComputerName% This is a popup message.
+:: NET SEND %ComputerName% This is a popup message.
 
-NET SEND %ComputerName%
-error: you already have a python interpreter installed on your system.
-Unfortunatly the interpreter version is not compatible with the project.
-You may consider to remove your python interpreter in, so the installer is able to install the proper Python version!"
+:: NET SEND %ComputerName%
+:: error: you already have a python interpreter installed on your system.
+:: Unfortunatly the interpreter version is not compatible with the project.
+:: You may consider to remove your python interpreter in, so the installer is able to install the proper Python version!"
 
 
-if exist C:\Python34
-    GOTO RESOURCES
-    GOTO INTERPRETERCHECK
+:: if exist C:\Python34
+::     GOTO RESOURCES
+::     GOTO INTERPRETERCHECK
 
-:INTERPRETERCHECK
-IF DEFINED Python
-    GOTO MALPYTHONINSTALLATION
-    GOTO INSTALLPYTHON
+:: :INTERPRETERCHECK
+:: IF DEFINED Python
+::     GOTO MALPYTHONINSTALLATION
+::     GOTO INSTALLPYTHON
 
-:MALPYTHONINSTALLATION
-echo your python interpreter is not matching the project resources
-start "" /wait cmd /c "echo error: you already have a python
-interpreter installed on your system.
-Unfortunatly the interpreter version is not compatible with
-the project. You may consider to remove your python interpreter in
-, so the installer is able to install the proper Python version!&echo(&pause"
+:: :MALPYTHONINSTALLATION
+:: echo your python interpreter is not matching the project resources
+:: start "" /wait cmd /c "echo error: you already have a python
+:: interpreter installed on your system.
+:: Unfortunatly the interpreter version is not compatible with
+:: the project. You may consider to remove your python interpreter in
+:: , so the installer is able to install the proper Python version!&echo(&pause"
 
-:INSTALLPYTHON
-echo the installer takes care on a fresh python interpreter installation
-call python-3.4.4.msi
+:: :INSTALLPYTHON
+:: echo the installer takes care on a fresh python interpreter installation
+:: call python-3.4.4.msi
 
 :RESOURCES
-echo interpreter installed, we now take care on the project resources
+:: echo interpreter installed, we now take care on the project resources
 easy_install demjson-2.2.4-py3.4.egg
 pip install decorator-4.0.6-py2.py3-none-any.whl
 pip install networkx-1.10.tar.gz
