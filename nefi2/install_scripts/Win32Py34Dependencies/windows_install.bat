54adf4a383b8803328dbfc8264afd86c641afc6e
Dennis Groß <gdennis91@googlemail.com>
2016-03-04 10:53:03 +0100

    changes

--- nefi2/install_scripts/Win32Py34Dependencies/windows_install.bat
+++ nefi2/install_scripts/Win32Py34Dependencies/windows_install.bat
@@ -8,5 +8,8 @@ pip install opencv_python-3.1.0-cp34-none-win32.whl
 pip install numpy-1.10.4+mkl-cp34-none-win32.whl
 easy_install thinning-1.2.3-py3.4-win32.egg
 start PyQt5-5.5.1-gpl-Py3.4-Qt5.5.1-x32.exe
+pip install Sphinx-1.2py33-none-any.whl
+easy_install QDarkStyle-2.1-py3.4.egg
+
 
 
